1e21d6fe59ed3b930e831137e3d4d7318b6a7621
teruto725 <akiteruto@gmail.com>
2019-12-12 18:03:41 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,156 @@
+# 金魚個体識別プログラム
+
+
+#  入出力
+1. adding_mode():金魚追加モードを移行する
+2. adding_fish(frame_no,name,rect):金魚を追加する
+3. delete_fish(fish_id):対象の金魚情報を削除する
+4. get_latest_frame():最新フレームを取り出す
+
+
+
+# 状態遷移
+
+## Tracking状態(adding_mode="NO")
+
+- trackingを行い追跡物の画像を対象のObjectクラスにaddImageTemp()する。
+
+- 画像データにtrackingによって求められた矩形領域並びに、対応しているobject名を書き込み、ストリーミングを行う。
+
+
+
+## Tracking状態(adding_mode="YES")
+
+- trackingを行い追跡物の画像,フレーム番号、矩形座標を対象のNameLessObjectクラスにaddInfo()する。
+
+
+
+## 新規金魚追加タイミング
+
+- adding_modeフラグを"YES"にする
+##  金魚名付けタイミング(adding_mode="YES")
+
+- 受け取った座標位置、flame番号が合致するNameLessObjectListを探索し、合致するNamelessObjectと受け取ったNameから新しいObjectクラスを生成する
+
+- adding_mode = "DONE"
+
+- 強制的に認識物消失タイミングに遷移
+
+
+
+## 認識物出現タイミング (adding_mode="NO")
+
+- CNNに出現物のデータを投げ、判別されたIDから出現したObjectを特定する。
+
+- trackerを出現物を対象として生成し、そのtrackerと特定したObjectを紐づける。
+
+- trackerによって切り取られた矩形画像を対応したObjectにaddImageTemp()する
+
+
+
+## 認識物出現タイミング (adding_mode="YES")
+
+- 新しいNameLessObjectを生成しそれとTrackerを紐づける
+
+
+
+
+## 認識物消失タイミング (adding_mode="YES")
+
+- 消失したNameLessObjectのTrackerを削除する
+
+
+
+## 認識物消失タイミング（adding_mode="DONE")
+
+- すべてのObjectに対してsaveImageTemp()する
+
+- 現在のCNNを削除し、新しくCNNをObject数に合わせて作成する
+
+- すべてのObjectに対してgetLearnAll()を行い取得したデータでCNNを学習させる
+
+- adding_mode="NO"
+
+
+
+## 認識物消失タイミング(adding_mode="NO")
+
+- 全オブジェクトに対してgetLearnTemp()を行い取得したデータでCNNを学習させる。
+
+- 全オブジェクトに対してsaveImageTemp()を行うことで学習した画像をimagelistに移す
+
+
+
+# 制約
+
+- 初期状態では金魚は水槽内にいないものとする
+
+- 複数同時に新しい金魚を登録することはできない
+
+- 名前付けは1匹ずつで行い必ず異なる金魚に対して行う。（金魚の数と、本プログラム内の金魚の数がずれたら終わり）
+
+- 認識されていない金魚は金魚追加モードがoffの時は0匹,onの時は1匹となっていなければならない
+
+
+
+# クラス
+
+## 1. Objectクラス
+
+### - 概要
+
+認識を行うオブジェクト
+
+### - 属性
+
+- tracker : トラッカー
+
+- id : CNNの目標値となるID
+
+- name : ユーザによって入力される正解
+
+- imagelist : trackingによって収集された過去の画像群
+
+- imagelist_temp : trackingによって収集された出現期間中の画像群
+
+
+
+### - ふるまい
+
+- init(NameLessObject,name) : NameLessObjectとnameを受け取ってname,imagelist,を更新する。
+- getLearnTemp() : ImageTempとIDを学習用に成形して渡す
+- getLearnAll()：Imagelistを学習用に成形して渡す
+
+- addImageTemp(image) :imagelist_tempに画像を追加する
+
+- saveImageTemp()：imagelist_tempをimagelistに移す
+
+
+
+## 2. NameLessObjectクラス
+
+### - 概要
+
+認識が完了していないオブジェクト（各リストはindexを共通させておく）
+
+### - 属性
+
+- frame_nolist : 画面に入ったときから出るときまでのframe番号
+
+- rectlist : 矩形座標リスト
+
+- imagelist: 矩形画像リスト
+
+- tracker : トラッカー
+
+### - ふるまい
+
+- addInfo(frame_no, rect, image) : それぞれのパラメータを追加する
+
+- addTracker(tracker) : トラッカーを追加する
+
+- tracking(frame, frame_no) :
+<!--stackedit_data:
+eyJoaXN0b3J5IjpbLTY4NDgyMDU0MCwtMTMyNTQ4NzY1OCwtOD
+UxNDUxMzE1LC0xODA1MjYwNTY0LDczMDk5ODExNl19
+-->
---
+++ kingyo.py
@@ -0,0 +1,262 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+from chainer import Sequential
+import chainer.functions as F
+import chainer.links as L
+import chainer
+from pathlib import Path
+from chainer import Chain, optimizers, Variable, serializers
+
+class NameLessObject():
+    def __init__(self):
+        self.frame_nolist = list()
+        self.rectlist = list()
+        self.imagelist = list()
+
+    def addTracker(self,tracker):
+        self.tracker = tracker
+
+    def rmTracker(self):
+        self.tracker = None
+
+    def tracking(self,frame,frame_no):#フレーム画像とフレーム番号を受け取ってtrackerを更新、認識できたかどうかを返す
+        self.frame_nolist.append(frame_no)
+        success,rect = self.tracker.update(frame)
+        if success:
+            rect = np.array(rect,dtype=np.int32)
+            self.rectlist.append(rect)
+            self.imagelist.append(frame[rect[1]:rect[1]+rect[3],rect[0]:rect[0]+rect[2]])
+        return success
+
+    def check(self,frame_no,point):#frame_no,point座標に自身が該当しているかどうか
+        if frame_no in self.frame_nolist:
+            rect = self.rectlist[self.frame_nolist.index(frame_no)]
+            if rect[0]<point[0]<rect[0]+rect[2] and rect[1]<point[1]<rect[1]+rect[3]:
+                return True
+        return False
+
+class Object():
+    obj_con = 0#objのidカウント用クラス変数
+    def __init__(self,name,imagelist):
+        self.name = name
+        self.imagelist= list()
+        self.imagelist = imagelist
+        self.id = Object.obj_con
+        Object.obj_con += 1
+        self.rectlist = list()
+        self.imagelist_temp = list()
+
+
+    def addTracker(self,tracker):
+        self.tracker = tracker
+
+    def rmTracker(self,tracker):
+        self.tracker = None
+
+    def tracking(self,frame,frame_no):#フレーム画像とフレーム番号を受け取ってtrackerを更新、認識できたかどうかを返す
+        self.frame_nolist.append(frame_no)
+        success,rect = self.tracker.update(frame)
+        if success:
+            rect = np.array(rect,dtype=np.int32)
+            self.rectlist.append(rect)
+            self.imagelist_temp.append(frame[rect[1]:rect[1]+rect[3],rect[0]:rect[0]+rect[2]])
+        return success
+
+    def saveImageTemp(self):
+        self.imagelist.append(self.imagelist_temp)
+    def getImageTemp(self):
+        return self.imagelist_temp
+    def getImagelist(self):
+        return self.imagelist
+    def getId(self):
+        return self.id
+#CNN
+class CNN(Chain):#出力数を受け取ってcnnを作成する
+    def __init__(self,output_num):
+        super(CNN, self).__init__(
+            conv1 = L.Convolution2D(3, 20, 5), # filter 5
+            conv2 = L.Convolution2D(20, 50, 5), # filter 5
+            l1 = L.Linear(800, 500),
+            l2 = L.Linear(500, 500),
+            l3 = L.Linear(500, output_num, initialW=np.zeros((output_num, 500), dtype=np.float32))
+        )
+    def forward(self, x):
+        h = F.max_pooling_2d(F.relu(self.conv1(x)), 2)
+        h = F.max_pooling_2d(F.relu(self.conv2(h)), 2)
+        h = F.relu(self.l1(h))
+        h = F.relu(self.l2(h))
+        h = self.l3(h)
+        return h
+
+#フレームを受け取り矩形領域リストを取り出す
+def getRectList(frame):
+    f_frame = frame.astype(np.uint8)#int変換
+    bulr_img =cv2.GaussianBlur(f_frame,(15,15), 0)#平滑化
+    gray_img = cv2.cvtColor(bulr_img, cv2.COLOR_RGB2GRAY)#グレースケール
+    threshold_value = 100
+    ret, thresh_img = cv2.threshold(gray_img, threshold_value, 255, cv2.THRESH_BINARY)#二値化
+    contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #fc_img,contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)#version依存
+    rect_list = list()#矩形領域の座標リスト
+    for layer in range(5):#layer = 階層の深さ
+        for i in range(0, len(contours)):
+            if len(contours[i]) > 0:
+                if hierarchy[0][i][3] != layer:
+                    continue
+                rect = contours[i]
+                x, y, w, h = cv2.boundingRect(rect)
+                rect_list.append([x,y,w,h])
+        if len(rect_list) != 0:#矩形領域を抽出できたら
+            break
+    else:#矩形領域を抽出できなかったら
+        return []
+    return rect_list
+
+#CNNように画像を調整
+def adjustRectImage(imagelist):
+    resize_imglist=list()
+    for i in range(len(imglist)):
+        resize_imglist.append(cv2.resize(imglist[i], (28,28),0,0 ,cv2.INTER_NEAREST))#28×28領域に調整
+    flat_imglist=list()
+    for i in range(len(resize_imglist)):
+        flat_imglist.append(resize_imglist[i].flatten())#2次元配列から1次元に変換
+    return flat_imglist
+
+#frameに認識結果を書き込みframeを返す
+def drawFrame(now_objlist,now_nlobjlist,frame):
+    for obj in now_objlist:
+        rect = obj.rectlist[-1]
+        cv2.putText(frame, str(obj.name), (rect[0], rect[1]-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 0), 2, 8)
+        cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 100, 0), 2)
+    for nlobj in now_nlobjlist:
+        rect = nlobj.rectlist[-1]
+        cv2.putText(frame, str("NameLess"), (rect[0], rect[1]-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 0), 2, 8)
+        cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 100, 0), 2)
+    return frame
+
+#新しくできた領域に対しtrackerを設定し、新たなNlobjを生成
+def appearNLObj(frame,frame_no,now_objlist,rect_list):
+    tracker = cv2.TrackerKCF_create()
+    tracker.init(frame, tuple(rect_list.pop(0)))
+    nlo = NameLessObject()
+    nlo.addTracker(tracker)
+    nlo.tracking(frame,frame_no)
+    now_nlobjlist.append(nlo)
+
+#画面上すべてのnloに対してtrackingを更新し金魚消失していないか調べる
+def renewTrackingNLObj(frame,frame_no,now_nlobjlist,past_nlobjlist,rect_list):
+    for nlobj in now_nlobjlist:#画面内に存在しているnlobj
+        success = nlobj.tracking(frame,frame_no)#
+        if success == False:#金魚消滅してたら
+            print("金魚消失")
+            nlobj.rmTracker()
+            now_nlobjlist.remove(nlobj)
+            past_nlobjlist.append(nlobj)
+
+#frame_noとprintから対象のNLOを特定し、nameとともにobj生成
+def naming(frame_no,now_objlist,now_nlobjlist,past_objlist,past_nlobjlist):
+    print("Naming")
+    print("Enter new Name:")
+    name = input()
+    #print("Enter Point:")
+    #point =list(map(int,input().split()))	#point = [x,y]
+    point = [rect_list[0][0]+10,rect_list[0][1]+10]#テスト用に座標を固定よってrect_listは引数に不必要
+    all_nlobjlist = list()
+    all_nlobjlist.extend(now_nlobjlist)
+    all_nlobjlist.extend(past_nlobjlist)
+    for nlobj in all_nlobjlist:
+        if nlobj.check(frame_no,point):
+            new_obj = Object(name,nlobj.imagelist)
+            if nlobj in now_nlobjlist:#new_objが現在画面内か、画面外かでappedn先が変わる
+                new_obj.addTracker(nlobj.tracker)
+                now_objlist.append(new_obj)
+            else:
+                past_objlist.append(new_obj)
+            break
+    else:
+        print("座標がずれています。もう一度指定しなおしてください")
+
+
+#初回の学習
+def createCNN(cnn,optimizer,now_objlist):
+    x_data = np.array((0,28*28), np.float32)#入力値
+    for obj in now_objlist:
+        np.append(x_data, np.array([adjustRectImage(obj.getImagelist())]))#怪しい
+
+
+def updateCNN(cnn,optimizer,train_img,id):
+    cnn
+
+
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(1) # 0はカメラのデバイス番号
+    now_objlist = list()#画面内のobj
+    past_objlist = list()#画面外のobj
+    now_nlobjlist = list()#画面内のnlobj
+    past_nlobjlist = list()#画面外のnl
+    adding_mode="OFF"
+    last_num = 0#前回フレームの金魚数
+    frame_no = 0
+
+    cnn = None #CNN
+    optimizer = None #optimizer
+
+    while True:
+        ret, frame = cap.read()#frame読み込み
+        rect_list = getRectList(frame)#画像内の矩形領域リスト
+        if adding_mode == "ON":
+            renewTrackingNLObj(frame,frame_no,now_nlobjlist,past_nlobjlist,rect_list)#tracker更新＋金魚消失判定
+            if len(now_nlobjlist) < len(rect_list):#認識されていない金魚がいるとき
+                print("金魚出現")
+                appearNLObj(frame,frame_no,now_objlist,rect_list)#NLObj生成
+
+        if adding_mode == "OFF":
+            #renewTrackingObj()
+            success_frag = False
+            for obj in now_objlist:#すべてのtrackerを更新する
+                success = obj.tracking(frame, frame_no)
+                if success == False:#消失
+                    print(name+":消滅")
+                    obj.rmTracker()
+                    now_objlist.remove(nlobj)
+                    past_objlist.append(nlobj)
+                    success_frag=True
+                    updateCNN(cnn,optimizer,obj.getImageTemp(),obj.getId())
+            if success_frag:#どれかが消滅したとき
+
+
+
+            if len(now_objlist) < len(rect_list):
+                tracker = cv2.TrackerKCF_create()
+                tracker.init(frame, tuple(rect_list.pop(0)))#0でいいの？
+                #NNつっこむ
+
+
+
+        drawFrame(now_objlist,now_nlobjlist,frame)#認識結果描画
+        cv2.imshow('camera capture', frame)#表示
+        k = cv2.waitKey(1) # 1msec待つ
+        if k == 13: #enterキーで追加モード
+            adding_mode = "ON"
+            print("adding_mode=ON")
+
+        if k == ord("g"):#gキーで名前登録
+            naming(frame_no,now_objlist,now_nlobjlist,past_objlist,past_nlobjlist)#名前からobject生成
+            all_objlist.extend(now_objlist)
+            all_objlist.extend(past_objlist)
+            cnn = CNN(len(all_objlist))
+            optimizer = chainer.optimizers.Adam()
+            optimizer.setup(model)
+            createCNN(cnn,optimizer)
+            adding_mode = "OFF"
+
+        if k == 27: # ESCキーで終了
+            break
+
+        frame_no += 1
+    cap.release()
+    cv2.destroyAllWindows()
